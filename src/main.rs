df3f3fe67767dc5b1a5267b91013cb61393cdc50
Justinas Staskevicius <staskevicius14@gmail.com>
2020-07-02 18:13:44 +0300

    uploaded everything

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use std::io::{Read, Write};
+
+const BUFFER_SIZE:u64 = 128 * 1024 * 1024;
+
+
+fn encode(start_dir: &str, output_dir: &str, password: &str) {
+    for (filename, dir) in get_files(start_dir) {
+        let mut file = std::fs::File::open(&filename).unwrap();
+        let _ = std::fs::create_dir_all(filename
+            .replacen(start_dir, output_dir, 1)
+            .replacen(&dir, "", 1));
+        let _ = std::fs::File::create(filename
+            .replacen(start_dir, output_dir, 1));
+        let mut output_file = std::fs::OpenOptions::new()
+            .append(true)
+            .open(filename
+                .replacen(start_dir, output_dir, 1))
+            .unwrap();
+        let total_len = file.metadata()
+            .unwrap()
+            .len();
+        let mut length = total_len;
+        loop {
+            let clen = if length < BUFFER_SIZE { length } else { BUFFER_SIZE };
+            if clen == 0 {
+                break;
+            }
+            length -= clen;
+            let mut buffer = vec![0u8; clen as usize];
+            let _ = file.read(&mut buffer);
+            let encrypted_data = encrypt(buffer, password);
+            let _ = output_file.write(&encrypted_data);
+        }
+    }
+}
+
+
+fn get_files(start_dir: &str) -> Vec<(String, String)> {
+    let mut output:Vec<(String, String)> = Vec::new();
+    for i in std::fs::read_dir(start_dir).unwrap() {
+        let ui = i.unwrap();
+        let md = match ui.metadata() {
+            Ok(x) => x,
+            Err(_) => continue
+        };
+        if md.is_file() {
+            if let Some(filename) = ui.file_name().to_str() {
+                output.push((ui.path().display().to_string(), filename.to_string()));
+            }
+        } else {
+            for j in get_files(&ui.path().display().to_string()) {
+                output.push(j);
+            }
+        }
+    }
+    output
+}
+
+
+fn encrypt(vector: Vec<u8>, password: &str) -> Vec<u8> {
+    let mut output = Vec::new();
+    for (i, j) in vector.iter().enumerate() {
+        let index = i % password.len();
+        let letter = match password[index..index + 1].chars().next() {
+            Some(x) => x as u8,
+            None => panic!()
+        };
+        output.push(j ^ letter as u8);
+    }
+    output
+}
+
+
+fn main() {
+    let args:Vec<String> = std::env::args().collect();
+    let sdir = args.get(1).expect("starting directory not specified");
+    let odir = args.get(2).expect("output directory not specified");
+    let pw = args.get(3).expect("password not specified");
+    let del_str = match args.get(4) {
+        Some(x)  => x as &str,
+        _ => ""
+    };
+    let del = if del_str.contains("del") {true} else {false};
+    encode(&sdir, &odir, &pw);
+    if del {
+        let _ = std::fs::remove_dir_all(sdir);
+    }
+}
