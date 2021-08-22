d6a306b4924e274a4482050fffa5526b391bdc99
Justinas Staskevicius <staskevicius14@gmail.com>
2021-08-22 13:37:58 +0300

    rewrote
    now encrypts files

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,107 @@
 use std::io::{Read, Write};
 
-const BUFFER_SIZE:u64 = 128 * 1024 * 1024;
+const BUFFER_SIZE: usize = 128 * 1024 * 1024;
+
+fn encrypt_file(i_filename: &str, o_filename: &str, password: &str, buffer: &mut Vec<u8>) {
+    let mut input_file = std::fs::OpenOptions::new().read(true).open(&i_filename).unwrap();
+    let mut output_file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(o_filename)
+        .unwrap();
+    let mut length = input_file.metadata()
+        .unwrap()
+        .len() as usize;
+    while length > 0 {
+        length -= if length > BUFFER_SIZE {
+            BUFFER_SIZE
+        } else {
+            buffer.resize(length, 0);
+            length
+        };
+        input_file.read(buffer).unwrap();
+        encrypt(buffer, password);
+        output_file.write(buffer).unwrap();
+    }
+}
 
 
 fn encode(start_dir: &str, output_dir: &str, password: &str) {
-    for (filename, dir) in get_files(start_dir) {
-        let mut file = std::fs::File::open(&filename).unwrap();
-        let _ = std::fs::create_dir_all(filename
-            .replacen(start_dir, output_dir, 1)
-            .replacen(&dir, "", 1));
-        let _ = std::fs::File::create(filename
-            .replacen(start_dir, output_dir, 1));
-        let mut output_file = std::fs::OpenOptions::new()
-            .append(true)
-            .open(filename
-                .replacen(start_dir, output_dir, 1))
+    let mut output_file_path;
+    let mut buffer= Vec::new();
+    let mut files = Vec::new();
+    get_files(start_dir, &mut files);
+    for (path, filename) in files {
+        output_file_path = path.replacen(start_dir, output_dir, 1);
+        std::fs::create_dir_all(output_file_path.replacen(&filename, "", 1))
             .unwrap();
-        let total_len = file.metadata()
-            .unwrap()
-            .len();
-        let mut length = total_len;
-        loop {
-            let clen = if length < BUFFER_SIZE { length } else { BUFFER_SIZE };
-            if clen == 0 {
-                break;
-            }
-            length -= clen;
-            let mut buffer = vec![0u8; clen as usize];
-            let _ = file.read(&mut buffer);
-            let encrypted_data = encrypt(buffer, password);
-            let _ = output_file.write(&encrypted_data);
-        }
+        encrypt_file(&path, &output_file_path, password, &mut buffer);
     }
 }
 
 
-fn get_files(start_dir: &str) -> Vec<(String, String)> {
-    let mut output:Vec<(String, String)> = Vec::new();
+fn get_files(start_dir: &str, output: &mut Vec<(String, String)>){
+    let mut ui;
+    let mut md= std::fs::metadata(start_dir).unwrap();
+    if md.is_file() {
+        output.push((start_dir.to_string(), String::with_capacity(0)));
+        return;
+    }
     for i in std::fs::read_dir(start_dir).unwrap() {
-        let ui = i.unwrap();
-        let md = match ui.metadata() {
-            Ok(x) => x,
-            Err(_) => continue
-        };
+        ui = i.unwrap();
+        md = ui.metadata().unwrap();
         if md.is_file() {
             if let Some(filename) = ui.file_name().to_str() {
-                output.push((ui.path().display().to_string(), filename.to_string()));
+                output.push((ui.path().to_str().unwrap().to_string(), filename.to_string()));
             }
         } else {
-            for j in get_files(&ui.path().display().to_string()) {
-                output.push(j);
-            }
+            get_files(ui.path().to_str().unwrap(), output);
         }
     }
-    output
 }
 
 
-fn encrypt(vector: Vec<u8>, password: &str) -> Vec<u8> {
-    if password.len() == 0 {
-        return vector;
+fn encrypt(vector: &mut Vec<u8>, password: &str) {
+    let password_len = password.len();
+    if password_len == 0 {
+        return;
     }
-    let mut output = Vec::new();
-    for (i, j) in vector.iter().enumerate() {
-        let index = i % password.len();
-        let letter = match password[index..index + 1].chars().next() {
-            Some(x) => x as u8,
-            None => panic!()
-        };
-        output.push(j ^ letter as u8);
+    let mut index;
+    for i in 0..vector.len() {
+        index = i % password_len;
+        vector[i] ^= password[index..index + 1].chars().next().unwrap() as u8;
     }
-    output
 }
 
 
 fn main() {
-    // -m moves the files back after encrypting them
-    // and also deletes the output directory
-    // -d deletes the files in the input directory
+    let flag;
     let args:Vec<String> = std::env::args().collect();
-    let sdir = args.get(1).expect("input directory not specified");
-    let odir = args.get(2).expect("output directory not specified");
-    if sdir == odir {
-        println!("cannon use the same directory");
-        panic!();
-    }
-    let pw = args.get(3).expect("password not specified");
-    let del_str = match args.get(4) {
-        Some(x)  => x as &str,
-        _ => ""
+    let sdir = args.get(1).expect("input directory not specified").trim_end_matches('/');
+    let odir = match args.get(3) {
+        Some(x) => {flag = false; x.trim_end_matches('/').to_string()},
+        None => {flag = true; format!("{}/xorencryptiontempfile", std::env::temp_dir().to_str().unwrap())}
     };
-    let overwrite = if del_str.contains("-m") {true} else {false};
-    let del = if del_str.contains("-d") {true} else {false};
-    encode(&sdir, &odir, &pw);
-    if del {
-        let _ = std::fs::remove_dir_all(sdir);
-    } else if overwrite {
-        let _ = std::fs::remove_dir_all(sdir);
+    assert_ne!(sdir, odir);
+    let password = args.get(2).expect("password not specified");
+
+    let md = std::fs::metadata(sdir).unwrap();
+    if md.is_file() {
+        let mut buffer = Vec::new();
+        if flag {
+            encrypt_file(sdir, &odir, password, &mut buffer);
+            std::fs::remove_file(sdir).unwrap();
+            encrypt_file(&odir, sdir, "", &mut buffer);
+            std::fs::remove_file(odir).unwrap();
+        } else {
+            encrypt_file(sdir, &odir, password, &mut buffer);
+        }
+    } else if flag {
+        encode(&sdir, &odir, password);
+        std::fs::remove_dir_all(sdir).unwrap();
         encode(&odir, &sdir, "");
-        let _ = std::fs::remove_dir_all(odir);
+        std::fs::remove_dir_all(odir).unwrap();
+    } else {
+        encode(&sdir, &odir, password);
     }
 }
